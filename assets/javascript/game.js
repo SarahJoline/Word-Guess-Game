14dc5af76a1fb52975d511b9b321df35e26c8d72
Sarah Naas <sjnaas@gmail.com>
2019-10-05 08:19:15 -0700

    Made it generate a random word that appears as underscores

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,19 @@
 var country = ['japan', 'china', 'belgium', 'brazil', 'cuba', 'senegal', 'colombia', 'turkey', 'greece', 'canada', 'france', 'italy'];
 var lives = 12;
 var wins = 0;
-var wordToGuess = [];
 
 remaining = document.getElementById("remaining");
 remaining.innerHTML = 'Guesses Remaining: ' + lives;
 
 score = document.getElementById("score");
 score.innerHTML = 'Wins: ' + wins;
+
+var word = country[Math.floor(Math.random() * country.length)];
+
+var wordToGuess = [];
+for (var i = 0; i < word.length; i++) {
+    wordToGuess[i] = "_";
+}
+
+var mysteryWord = document.getElementById('word');
+mysteryWord.innerHTML = wordToGuess.join(" ");
