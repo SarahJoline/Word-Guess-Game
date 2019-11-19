c95540581b1c6295e8ab13a607cbebd3651615ff
Sarah Naas <sjnaas@gmail.com>
2019-11-18 16:25:56 -0800

    got the game to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,120 @@
-var country = ['japan', 'china', 'belgium', 'brazil', 'cuba', 'senegal', 'colombia', 'turkey', 'greece', 'canada', 'france', 'italy'];
-var lives = 12;
+var alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+var country = [
+  "japan",
+  "china",
+  "belgium",
+  "brazil",
+  "cuba",
+  "senegal",
+  "colombia",
+  "turkey",
+  "greece",
+  "canada",
+  "france",
+  "italy"
+];
+
 var wins = 0;
+var losses = 0;
 
-remaining = document.getElementById("remaining");
-remaining.innerHTML = 'Guesses Remaining: ' + lives;
+restart();
 
-score = document.getElementById("score");
-score.innerHTML = 'Wins: ' + wins;
+function restart() {
+  var lives = 12;
+  var wordToGuess = [];
+  var incorrectGuesses = [];
 
-var word = country[Math.floor(Math.random() * country.length)];
+  var hiddenWord = country[Math.floor(Math.random() * country.length)];
+  console.log(hiddenWord);
 
-var wordToGuess = [];
-for (var i = 0; i < word.length; i++) {
-    wordToGuess[i] = "_";
+  var word = hiddenWord.split("");
+  console.log(word);
+
+  for (var i = 0; i < word.length; i++) {
+    wordToGuess.push("_");
+  }
+
+  console.log(wordToGuess);
+
+  document.querySelector("#word").innerHTML = wordToGuess.join(" ");
+  document.querySelector("#wins").innerHTML = wins;
+  document.querySelector("#losses").innerHTML = losses;
+
+  document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userGuess);
+    var doubles = incorrectGuesses.includes(userGuess);
+    var validKeys = alphabet.includes(userGuess);
+    if (doubles) {
+      return;
+    } else if (!validKeys) {
+      return;
+    } else {
+      game();
+    }
+
+    function game() {
+      var letterInWord = false;
+
+      if (word.includes(userGuess)) {
+        letterInWord = true;
+      }
+
+      if (letterInWord) {
+        for (var i = 0; i < word.length; i++) {
+          if (userGuess === word[i]) {
+            wordToGuess[i] = userGuess;
+            document.querySelector("#word").innerHTML = wordToGuess.join(" ");
+          }
+        }
+      } else {
+        lives--;
+        document.querySelector("#remaining").innerHTML = lives;
+        incorrectGuesses.push(userGuess);
+        console.log(incorrectGuesses);
+        document.querySelector(
+          "#wrong-guesses"
+        ).innerHTML = incorrectGuesses.join(", ");
+      }
+      if (lives == 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = losses;
+        restart();
+      }
+
+      if (!wordToGuess.includes("_")) {
+        wins++;
+        document.querySelector("#wins").innerHTML = wins;
+        restart();
+      }
+    }
+  };
 }
-
-var mysteryWord = document.getElementById('word');
-mysteryWord.innerHTML = wordToGuess.join(" ");
